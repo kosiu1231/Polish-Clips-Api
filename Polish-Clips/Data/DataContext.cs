8b714f36bd5a7075ee52f642f8148e20f185e02b
kosiu1231 <aroczek1@wp.pl>
2024-03-13 15:39:30 +0100

    Add DataContext

---
+++ Polish-Clips/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Polish_Clips.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Clip> Clips => Set<Clip>();
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<Game> Games => Set<Game>();
+        public DbSet<Like> Likes => Set<Like>();
+        public DbSet<Report> Reports => Set<Report>();
+    }
+}
