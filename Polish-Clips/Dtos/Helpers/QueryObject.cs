9e619258fdc3332da3fde2fb7c9cdb361049a70e
kosiu1231 <aroczek1@wp.pl>
2024-04-04 16:28:15 +0200

    Change QueryObject

--- Polish-Clips/Dtos/Helpers/QueryObject.cs
+++ Polish-Clips/Dtos/Helpers/QueryObject.cs
@@ -10,6 +10,6 @@
         public string? SortBy { get; set; } = null;
         public bool IsDescending { get; set; } = false;
         public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 20;
+        public int PageSize { get; set; } = 12;
     }
 }
--- Polish-Clips/Dtos/User/UserLoginRequest.cs
+++ Polish-Clips/Dtos/User/UserLoginRequest.cs
@@ -2,7 +2,7 @@
 {
     public class UserLoginRequest
     {
-        [Required, EmailAddress]
+        [Required]
         public string Email { get; set; } = string.Empty;
         [Required]
         public string Password { get; set; } = string.Empty;
